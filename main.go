c02d33160b04b488ddaf9683084e173df6930097
jeremy <jeremy.nowak@laplateforme.io>
2024-09-04 16:49:48 +0200

    feat: Add Fiber framework for web server and routes
    The code changes include adding the Fiber framework for creating a web server and defining routes. This will allow the application to handle HTTP requests and responses. The recent user commits indicate the addition of dependencies for Fiber and PostgreSQL, as well as the creation of a Docker compose file for containerization. The recent repository commits also show the addition of Fiber dependencies and updates to the README file.

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/gofiber/fiber/v2"
 	_ "github.com/lib/pq"
 )
 
 func main() {
+	// Connexion à la base de données PostgreSQL
 	connStr := "user=admin password=securepassword dbname=safebase sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
@@ -23,4 +25,23 @@ func main() {
 	}
 
 	fmt.Println("Successfully connected to PostgreSQL!")
+
+	// Création de l'application Fiber
+	app := fiber.New()
+
+	// Définition de la route "/"
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("The /!")
+	})
+
+	// Définition de la route "/hello"Ò
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+	// // Démarrage du serveur sur le port 8080
+	err = app.Listen(":8080")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+
 }
